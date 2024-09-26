8354eb3af206cd0db2f40626f283e680b7f19ea9
Wilson Fernandes <wilsonfernandes390@gmail.com>
2024-09-26 01:19:53 -0300

    Testes com pygame

--- Game/sprites.py
+++ Game/sprites.py
@@ -21,22 +21,38 @@ x, y = (0, 0)
 clock = pygame.time.Clock()
 
 while not game_over: 
-    dt = clock.trick(100)
+    dt = clock.tick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        elif event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
+            x -= spriteWidth/2
+            y -= spriteHeight/2
     pressed = pygame.key.get_pressed()
-    if pressed[K_UP]: # type: ignore
-        y -= 1
-    if pressed[K_DOWN]: # type: ignore
-        y += 1
-    if pressed[K_LEFT]: # type: ignore
-        x -= 1
-    if pressed[K_RIGHT]: # type: ignore
-        x += 1
+    if pressed[K_UP]:
+        y -= 0.5 * dt
+    if pressed[K_DOWN]:
+        y += 0.5 * dt
+    if pressed[K_LEFT]: 
+        x -= 0.5 * dt
+    if pressed[K_RIGHT]: 
+        x += 0.5 * dt
+    if pressed[K_SPACE]:
+        x = 0
+        y = 0
+    
+    if x > (screen.get_width() - spriteWidth):
+        x = screen.get_width() - spriteWidth
+    if y > (screen.get_height() - spriteHeight):
+        y = screen.get_height() - spriteHeight
+    if x < 0:
+        x = 0
+    if y < 0:
+        y = 0
+        
     screen.fill((0,0,0))
-    '''screen.blit(player, (screen.get_width() / 2 - spriteWidth / 2,
-                           screen.get_height() / 2 - spriteHeight / 2))'''
     screen.blit(player, (x, y))    
     pygame.display.update()
+    
 pygame.quit()
