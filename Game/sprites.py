0d842b021f84fdc7b0232a06a7e547d600d7cc52
Wilson Fernandes <wilsonfernandes390@gmail.com>
2024-09-25 23:58:07 -0300

    Testes com pygame

--- Game/sprites.py
+++ Game/sprites.py
@@ -18,7 +18,10 @@ game_over = False
 
 x, y = (0, 0)
 
+clock = pygame.time.Clock()
+
 while not game_over: 
+    dt = clock.trick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
@@ -31,6 +34,7 @@ while not game_over:
         x -= 1
     if pressed[K_RIGHT]: # type: ignore
         x += 1
+    screen.fill((0,0,0))
     '''screen.blit(player, (screen.get_width() / 2 - spriteWidth / 2,
                            screen.get_height() / 2 - spriteHeight / 2))'''
     screen.blit(player, (x, y))    
